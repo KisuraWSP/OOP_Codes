83b2ec39728da4c82ff9e67ff8c716fd08d44583
Kisura <85266425+KisuraWSP@users.noreply.github.com>
2023-03-10 20:14:51 +0530

    Create Test.java

---
+++ Tutorial/Tutorial3/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.input();
+		s.print();
+		s.printDetails();
+
+		Book b = new Book();
+		b.input();
+		b.print();
+		b.printDetails();
+	}
+
+}
